5aaac1a87cd3ab800be05b8d16ecae2855e0dc5c
김진희 <jinheekim@gimjinhuiui-MacBookPro.local>
2023-01-10 10:49:07 +0900

    root 경로 추가

---
+++ reactrouter-tutorial/src/routes/root.jsx
@@ -0,0 +1,40 @@
+export default function Root() {
+  return (
+    <>
+      <div id="sidebar">
+        <h1>React Router Contacts</h1>
+        <div>
+          <form id="search-form" role="search">
+            <input 
+              id="q"
+              aria-label="Search contacts"
+              placeholder="Search"
+              type="search"
+              name="q"
+            />
+            <div
+              id="search-spinner"
+              aria-hidden
+              hidden={true}
+            />
+            <div
+              className="sr-only"
+              aria-live="polite"
+            />
+          </form>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <a href={`contacts/1`}>Jinhee Kim</a>
+            </li>
+            <li>
+              <a href={`contacts/2`}>JiKang Um</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div id="detail"></div>
+    </>
+  );
+}
